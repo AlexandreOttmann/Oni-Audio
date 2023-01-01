960e3c85d7db20cb225a30c2e422c6e368d1968b
AlexandreOttmann <ottmann.alex@gmail.com>
2023-01-01 17:42:01 +0100

    Scroll Behavior, New section, correct img

--- app.js
+++ app.js
@@ -48,11 +48,6 @@ card.addEventListener('click', () => {
         duration: 1,
         ease: "expo.out",
         absolute: true,
-       
-    //  onComplete
-        
-
-
     })
 })
 });
@@ -77,3 +72,46 @@ muteButton.addEventListener('click', (function(){
 
 
 
+// ======== CARD HOVERING EFFECT
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
+
+
+
+//   SCROLLING HORIZONTAL
+
+var windowWidth = window.innerWidth;
+
+var horLength = document.querySelector(".element-wrapper").scrollWidth;
+var horLength2 = document.querySelector(".element-wrapper2").scrollWidth;
+
+var distFromTop = document.querySelector(".horizontal-section").offsetTop;
+var distFromTop2 = document.querySelector(".horizontal-section2").offsetTop;
+
+var scrollDistance = distFromTop + horLength - windowWidth;
+var scrollDistance2 = distFromTop2 + horLength2 - windowWidth;
+
+document.querySelector(".horizontal-section").style.height = horLength + "px";
+
+document.querySelector(".horizontal-section2").style.height = horLength2 + "px";
+
+window.onscroll = function(){
+  var scrollTop = window.pageYOffset;
+  
+  if (scrollTop >= distFromTop && scrollTop <= scrollDistance) {
+    document.querySelector(".element-wrapper").style.transform = "translateX(-"+(scrollTop - distFromTop)+"px)";
+  }
+  
+  if (scrollTop >= distFromTop2 && scrollTop <= scrollDistance2) {
+    document.querySelector(".element-wrapper2").style.transform = "translateX(-"+(scrollTop - distFromTop2)+"px)";
+  }
+}
